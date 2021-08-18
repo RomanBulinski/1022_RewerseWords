339516a34aee6a5bae8c3c467848fa970c18f2e7
bulir <aboandrus@gamil.com>
2021-08-18 20:42:20 +0200

    Code refactor

--- src/main/java/KataReverseWords.java
+++ src/main/java/KataReverseWords.java
@@ -1,21 +1,14 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class KataReverseWords {
 
     public static String reverseWords(final String original) {
-
         String[] words = original.split(" ");
-        List<String> reversedwords = new ArrayList<>();
-        for (String word : words) {
-            List<String> reverseLetters = new ArrayList<>();
-            String[] letters = word.split("");
-            for (int i = letters.length - 1; i > -1; i--) {
-                reverseLetters.add(letters[i]);
-            }
-            String reverseWord = String.join("", reverseLetters);
-            reversedwords.add(reverseWord);
-        }
-        return String.join(" ", reversedwords);
+        List<String> reverencedWords = Arrays.asList(words).stream().map( word -> new StringBuilder(word).reverse().toString() ).collect(Collectors.toList());
+        return String.join(" ", reverencedWords);
     }
+
 }
